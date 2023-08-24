d0f5d1ce2ba5f43386b4c347fbac41a92bcf63aa
SekohAndy <abeoabeo17@gmail.com>
2023-08-24 14:42:15 +0000

    CSIR work

--- js/map.js
+++ js/map.js
@@ -1,10 +1,10 @@
-var map = L.map('map').setView([5.594951,-0.18503], 17);
+var map = L.map('map').setView([5.595883,-0.187689], 17);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-var marker = L.marker([5.594951,-0.18503]).addTo(map)
+var marker = L.marker([5.595883,-0.187689]).addTo(map)
 		.bindPopup('The Technology Sandbox<br> Where Andy is sitting this very moment')
 		.openPopup();	
 
